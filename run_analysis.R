b791caef15e7a67aadc901e847013bcc02b8445c
zzachzz <zchristopher794@gmail.com>
2021-07-06 16:10:01 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,23 +5,23 @@ library(data.table)
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
 #file destination
-destfile <- "C:/Users/Zach/Desktop/URI HAR Dataset.zip"
+destfile <- "/URI HAR Dataset.zip"
 
 #download zip file
 download.file(url, destfile)
 
 #unzip
-unzip("C:/Users/Zach/Desktop/URI HAR Dataset.zip",list=TRUE)
+unzip("/URI HAR Dataset.zip",list=TRUE)
 
 #filepaths assigned to variables
-trainFile <- "C:/Users/Zach/Desktop/UCI HAR Dataset/train/X_train.txt"
-testFile <- "C:/Users/Zach/Desktop/UCI HAR Dataset/test/X_test.txt"
-testLabel <- "C:/Users/Zach/Desktop/UCI HAR Dataset/test/y_test.txt"
-trainLabel <- "C:/Users/Zach/Desktop/UCI HAR Dataset/train/y_train.txt"
-activLabel <- "C:/Users/Zach/Desktop/UCI HAR Dataset/activity_labels.txt"
-features <- "C:/Users/Zach/Desktop/UCI HAR Dataset/features.txt"
-subjectTrain <- "C:/Users/Zach/Desktop/UCI HAR Dataset/train/subject_train.txt"
-subjectTest <- "C:/Users/Zach/Desktop/UCI HAR Dataset/test/subject_test.txt"
+trainFile <- "UCI HAR Dataset/train/X_train.txt"
+testFile <- "UCI HAR Dataset/test/X_test.txt"
+testLabel <- "UCI HAR Dataset/test/y_test.txt"
+trainLabel <- "UCI HAR Dataset/train/y_train.txt"
+activLabel <- "UCI HAR Dataset/activity_labels.txt"
+features <- "UCI HAR Dataset/features.txt"
+subjectTrain <- "UCI HAR Dataset/train/subject_train.txt"
+subjectTest <- "UCI HAR Dataset/test/subject_test.txt"
 
 #read in datasets from each text file
 train <- read.table(trainFile) #train values
@@ -45,32 +45,24 @@ head(trainTest)
 #add activity value column to dataset
 allLabels <- rbind(trainLabelTable,testLabelTable) #combine label tables
 trainTestWLabels <- cbind(trainTest,allLabels) #combine labels with data
-head(trainTestWLabels)
 
 ##transpose features measurement descriptions
 transp <- t(featuresTable$V2)
 transp <- data.frame(transp)
 
-##self-check if column names are assigned properly with the following 2 instructions 
-colnames(transp) <- transp[,1:561]
-names(transp)
-
 #assign accurate column names for dataset
 colnames(trainTestWLabels) <- transp[,1:561]
 names(trainTestWLabels)[is.na(names(trainTestWLabels))] <- "activity"
-head(trainTestWLabels)
-names(trainTestWLabels)
 
 
 #replace activity numbers with verb activities
 trainTestWLabels$activity <- gsub(pattern="^[1-6]",trainTestWLabels$activity,activLabelTable[trainTestWLabels$activity,2])
-head(trainTestWLabels$activity)
-colnames(trainTestWLabels)
+
 
 #add subject column to dataset
 trainTestWLabels <- cbind(trainTestWLabels,allSubjects)
 colnames(trainTestWLabels)[(names(trainTestWLabels)) == "V1"] <- "subject"
-head(trainTestWLabels)
+
 
 #select columns measuring mean (meanFreq() will need to be removed from dataset)
 meanCols <- grep(pattern="-mean()",colnames(trainTestWLabels))
@@ -85,22 +77,17 @@ stdCols <- grep(pattern="-std()",colnames(trainTestWLabels))
 
 #move mean and std values together into new dataframe
 meanStdVals <- cbind(trainTestWLabels[meanCols],trainTestWLabels[stdCols])
-head(meanStdVals)
-names(meanStdVals)
 
 
 #create new dataset to compute avg of each var for each activity and each subject
 meanStdVals <- cbind(meanStdVals, trainTestWLabels$activity)
 meanStdVals <- cbind(meanStdVals, trainTestWLabels$subject)
-names(meanStdVals)
 
 
 #compute the means across dataset
 tidyMeansStd <- meanStdVals %>%
   group_by(trainTestWLabels$activity,trainTestWLabels$subject) %>%
   summarize_all(mean)
-
-head(tidyMeansStd)
 
 #write out tidy dataset
 write.table(tidyMeansStd,"C:/Users/Zach/Desktop/neat.txt",row.names = FALSE)
